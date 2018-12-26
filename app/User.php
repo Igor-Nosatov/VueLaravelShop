3dac0656d15506d4feb96b8108a3a1e2fb957b54
Igor-Nosatov <sinevor13@gmail.com>
2018-12-26 13:59:22 +0200

    api tokens

--- app/User.php
+++ app/User.php
@@ -4,9 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
   class User extends Authenticatable
   {
+     use  HasApiTokens;
 
       protected $fillable = [
           'name', 'email', 'password',
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,12 +4,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    
+
     public function run()
     {
          $this->call(BrandTableSeeder::class);
          $this->call(CategoryTableSeeder::class);
          $this->call(ColorTableSeeder::class);
          $this->call(ProductTableSeeder::class);
+         $this->call(UserTableSeeder::class);
     }
 }
