e37a328ca8a153571193cc1c2be37528d1e42bf3
Igor-Nosatov <sinevor13@gmail.com>
2018-12-27 16:03:07 +0200

    add routing to vue.js

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <header-component></header-component>
+       <router-view></router-view>
+       <deals-component></deals-component>
+       <footer-component></footer-component>
+
+    </div>
+</template>
+
+
+<script>
+
+    export default{
+    }
+</script>
---
+++ resources/js/routes/routing.js
@@ -0,0 +1,11 @@
+import HomeComponent from '../components/HomeComponent.vue';
+
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: HomeComponent
+  }
+];
+
+export default routes
